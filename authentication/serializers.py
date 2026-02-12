d8434fbdeff858c893e35e7b837f1b67dfaa02e3
Shalin Nimesha Malshan <shalinnimesha8@gmail.com>
2026-02-12 22:27:06 +0530

    add features
    Removed commented-out code and unnecessary blank lines.

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -33,16 +33,12 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         # Add custom claims/user info to the token payload if needed
         token['username'] = user.username
         # token['role'] = user.role # If you have a custom User model
-
         
-
         return token
 
     def validate(self, attrs):
         # The parent validation handles authentication (username/password check)
         data = super().validate(attrs)
-
-
         
         # Include user details in the login response
         data['user'] = {
